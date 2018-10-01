eaae0e4f5ccf56c670002c16619c32bc37ed2c59
“MiguelFor” <miguelfor@hotmail.com>
2018-10-01 11:59:39 -0500

    first commit

---
+++ app/src/main/java/com/nextuniversity/configuraciongaleria/MainActivity.java
@@ -0,0 +1,16 @@
+package com.nextuniversity.configuraciongaleria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
